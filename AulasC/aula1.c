c372701bca8287780c85d7e9ef7393c6ea033369
Nitinh0 <rui.jesus27@gmail.com>
2026-06-13 17:19:38 +0100

    Estrutura inicial do projeto e Aula 1

---
+++ AulasC/aula1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int ano = 2026;
+    double preco_cafe = 0.55;
+    char inicial = 'R';
+
+    printf("Ano atual: %d\n", ano);
+    printf("Preco de um cafe: %.2f\n", preco_cafe);
+    printf("Inicial do Nome: %c\n", inicial);
+
+    return 0;
+}
