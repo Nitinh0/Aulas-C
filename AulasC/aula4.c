32cad6a5709ce83f989b6cfec6a2f98a03746d4d
Nitinh0 <rui.jesus27@gmail.com>
2026-06-13 18:18:05 +0100

    Aula 4: O Próximo Nível da Organização — Funções

---
+++ AulasC/aula4.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void verificarOrcamento(double custo) {
+	if (custo > 5000.0) {
+		printf("Alerta: Projeto de Alto Custo!\n");
+	}
+	else {
+		printf("Projeto dentro do orçamento base.\n");
+	}
+}
+int main() {
+	double custo = -0.1;
+
+	while (custo != 0) {
+		printf("Digite o custo do projeto:\n");
+		scanf("%lf", &custo);
+
+		if (custo != 0) {
+			verificarOrcamento(custo);
+		}
+	}
+}
